cbda231ff6ffa65d060a329c7e06d6dc01337f02
lhkingkong <lhkingkong@gmail.com>
2018-07-04 17:22:10 -0500

    Finish all the details

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,18 +10,33 @@
 angular.module('photosApp')
   .controller('MainCtrl', function ($scope, photos) {
   console.log('main', photos);
-    //$scope.photos = photos;
-  var latest3Albums = [];
-  var albums = {};
+  
+    var albums = {};
+    var latest = 0;
     for(var i = 0, len = photos.length; i<len; i++){
       if(!albums[photos[i].albumId]){
         albums[photos[i].albumId]=[];
       }
       albums[photos[i].albumId].push(photos[i]);
+      if(latest < photos[i].albumId){
+        latest = photos[i].albumId;
+      }
     }
   
   $scope.albums = albums;
+
+    $scope.newAlbums = [$scope.albums[latest]];
   
-  console.log(albums);
+    while($scope.newAlbums.length<3 && latest >0){
+      latest--;
+      if($scope.albums[latest]){
+        $scope.newAlbums.push($scope.albums[latest]);
+      }
+    }
   
+    
+  
+  
+  
+    console.log(albums);
   });
